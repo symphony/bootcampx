3e94d5074b34f6340852cf90d58a50ee063a1741
symphony <jciurc@gmail.com>
2022-05-02 15:50:50 -0600

    add new assignment and assignment submission entities. inserts example assignment seeds

---
+++ README.md
@@ -0,0 +1,3 @@
+# BootcampX
+
+Creates a Database in PostgreSQL which holds mock data of Cohorts and Students. 
--- migrations/students_cohorts.sql
+++ migrations/students_cohorts.sql
@@ -1,4 +1,4 @@
-DROP TABLE IF EXISTS cohorts, students;
+DROP TABLE IF EXISTS cohorts, students, assignments, assignment_submissions;
 
 CREATE TABLE cohorts (
   id INTEGER PRIMARY KEY NOT NULL,
@@ -17,3 +17,25 @@ CREATE TABLE students (
   end_date DATE,
   cohort_id INTEGER REFERENCES cohorts(id) ON DELETE CASCADE
 );
+
+CREATE TABLE assignments (
+  id INTEGER PRIMARY KEY NOT NULL,
+  name VARCHAR(255),
+  content TEXT,
+  day INTEGER,
+  chapter INTEGER,
+  duration INTEGER
+);
+
+CREATE TABLE assignment_submissions (
+  id INTEGER PRIMARY KEY NOT NULL,
+  assignment_id INTEGER REFERENCES assignments(id) ON DELETE CASCADE,
+  student_id INTEGER REFERENCES students(id) ON DELETE CASCADE,
+  duration INTEGER,
+  submission_date DATE
+);
+
+\i seeds/cohorts.sql
+\i seeds/students.sql
+\i seeds/assignments_seeds.sql
+\i seeds/assignment_submissions_seeds.sql
