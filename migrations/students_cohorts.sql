2679f067354ac3e3058b3dc3f096b33f5367347a
symphony <jciurc@gmail.com>
2022-05-03 17:01:22 -0600

    add 2 new tables for teachers and assistance_requests

--- migrations/students_cohorts.sql
+++ migrations/students_cohorts.sql
@@ -1,14 +1,15 @@
+-- -- deletes all data and rebuilds tables. only use if necessary
 -- DROP TABLE IF EXISTS cohorts, students, assignments, assignment_submissions;
 
 CREATE TABLE cohorts (
-  id INTEGER PRIMARY KEY NOT NULL,
+  id SERIAL PRIMARY KEY NOT NULL,
   name VARCHAR(255) NOT NULL,
   start_date DATE,
   end_date DATE
 );
 
 CREATE TABLE students (
-  id INTEGER PRIMARY KEY NOT NULL,
+  id SERIAL PRIMARY KEY NOT NULL,
   name VARCHAR(255) NOT NULL,
   email VARCHAR(255),
   phone VARCHAR(32),
@@ -19,7 +20,7 @@ CREATE TABLE students (
 );
 
 CREATE TABLE assignments (
-  id INTEGER PRIMARY KEY NOT NULL,
+  id SERIAL PRIMARY KEY NOT NULL,
   name VARCHAR(255),
   content TEXT,
   day INTEGER,
@@ -28,13 +29,34 @@ CREATE TABLE assignments (
 );
 
 CREATE TABLE assignment_submissions (
-  id INTEGER PRIMARY KEY NOT NULL,
+  id SERIAL PRIMARY KEY NOT NULL,
   assignment_id INTEGER REFERENCES assignments(id) ON DELETE CASCADE,
   student_id INTEGER REFERENCES students(id) ON DELETE CASCADE,
   duration INTEGER,
   submission_date DATE
 );
 
+CREATE TABLE teachers (
+  id SERIAL PRIMARY KEY NOT NULL,
+  name VARCHAR(255),
+  start_date DATE,
+  end_date DATE,
+  is_active BOOLEAN DEFAULT true
+);
+
+CREATE TABLE assistance_requests (
+  id SERIAL PRIMARY KEY NOT NULL,
+  assignment_id INTEGER REFERENCES assignments(id) ON DELETE CASCADE,
+  teacher_id INTEGER REFERENCES teachers(id) ON DELETE CASCADE,
+  student_id INTEGER REFERENCES students(id) ON DELETE CASCADE,
+  created_at DATE,
+  started_at DATE,
+  student_feedback TEXT,
+  teacher_feedback TEXT
+);
+
+
+-- -- populate tables with example data
 -- \i seeds/cohorts.sql
 -- \i seeds/students.sql
 -- \i seeds/assignments_seeds.sql
