43285c4586a9f0e820deec7b27788d45ce397bb9
symphony <jciurc@gmail.com>
2022-05-04 19:52:56 -0600

    set up the javascript project

--- 4_queries/13_name_of_teachers_and_number_of_assistances.sql
+++ 4_queries/13_name_of_teachers_and_number_of_assistances.sql
@@ -1,8 +1,9 @@
-SELECT teachers.name as teacher, max(cohorts.name) as cohort, count(assistance_requests.*) as total_assistances
+SELECT teachers.name as teacher, cohorts.name as cohort, count(assistance_requests) as total_assistances
 FROM cohorts
 JOIN students ON cohort_id = cohorts.id
 JOIN assistance_requests ON student_id = students.id
 JOIN teachers ON teachers.id = teacher_id
 WHERE cohorts.name = 'JUL02'
-GROUP BY teacher
+GROUP BY teacher, cohort
+ORDER BY teacher
 ;
